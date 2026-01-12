e360bc751f3db573c34395cbd698689f52a337e5
erhankrasniqi <erhankrasniqi@yahoo.com>
2026-01-12 20:13:07 +0100

    service collection edit

--- UserManagement.API/Program.cs
+++ UserManagement.API/Program.cs
@@ -1,9 +1,6 @@
  
 using UserManagement.API.Middlewares;
 using UserManagement.API;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using Infrastructure.Messaging.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,84 +10,14 @@ string corsPolicy = "TapyPayPolicy";
 builder.Services.AddCorsInApplication(corsPolicy);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerWithJwtSupport();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddProblemDetails();
 builder.Services.InitializeServices(builder.Configuration);
 builder.Services.AddMessaging(builder.Configuration);
-
-
-builder.Services.AddSwaggerGen(options =>
-{ 
-    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Vendos tokenin tënd këtu (pa 'Bearer ')"
-    });
-
-    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
-
-
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = "http://localhost:8080/realms/master";
-        options.RequireHttpsMetadata = false; // Çaktivizo kërkesën për HTTPS gjatë zhvillimit
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = "http://localhost:8080/realms/master",
-            ValidAudiences = new[] { "master-realm", "TapyPay-realm", "account", "user-management-api" },
-            ValidateAudience = true,
-            ValidateIssuer = true
-        };
-
-
-        options.Events = new JwtBearerEvents
-        {
-            OnAuthenticationFailed = context =>
-            {
-                Console.WriteLine("AUTHENTICATION FAILED:");
-                Console.WriteLine(context.Exception.Message);
-                return Task.CompletedTask;
-            },
-            OnTokenValidated = context =>
-            {
-                Console.WriteLine("TOKEN VALIDATED:");
-                Console.WriteLine($"User: {context.Principal.Identity?.Name}");
-                return Task.CompletedTask;
-            },
-            OnChallenge = context =>
-            {
-                Console.WriteLine("AUTH CHALLENGE:");
-                Console.WriteLine(context.Error);
-                Console.WriteLine(context.ErrorDescription);
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-
+builder.Services.AddAuthenticationKeyCloak();
 
 builder.Services.AddAuthorization();
 
--- UserManagement.API/ServiceCollectionExtensions.cs
+++ UserManagement.API/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using UserManagement.Application;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using UserManagement.Application;
 using UserManagement.Infrastructure;
 namespace UserManagement.API
 {
@@ -27,6 +30,83 @@ namespace UserManagement.API
             services.RegisterDbContext(dbConnectionString);
             services.RegisterRepositories();
             services.AddCqrs();
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwaggerWithJwtSupport(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Vendos tokenin tënd këtu (pa 'Bearer ')"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+        
+        public static IServiceCollection AddAuthenticationKeyCloak(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "http://localhost:8080/realms/master";
+                options.RequireHttpsMetadata = false; // Çaktivizo kërkesën për HTTPS gjatë zhvillimit
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = "http://localhost:8080/realms/master",
+                    ValidAudiences = new[] { "master-realm", "TapyPay-realm", "account", "user-management-api" },
+                    ValidateAudience = true,
+                    ValidateIssuer = true
+                };
+
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("AUTHENTICATION FAILED:");
+                        Console.WriteLine(context.Exception.Message);
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("TOKEN VALIDATED:");
+                        Console.WriteLine($"User: {context.Principal.Identity?.Name}");
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        Console.WriteLine("AUTH CHALLENGE:");
+                        Console.WriteLine(context.Error);
+                        Console.WriteLine(context.ErrorDescription);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
             return services;
         }
